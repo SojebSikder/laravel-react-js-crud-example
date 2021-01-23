c30f593ba66c872b5a85924f6edeef299b8c4dd7
sojebsikder <sojebsikder@gmail.com>
2021-01-23 17:44:07 +0600

    main

---
+++ public/js/resources_js_Pages_home_index.js
@@ -0,0 +1,13 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Pages_home_index"],{
+
+/***/ "./resources/js/Pages/home/index":
+/*!***************************************!*\
+  !*** ./resources/js/Pages/home/index ***!
+  \***************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
