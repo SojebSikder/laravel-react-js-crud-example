3d9727c2df2d8774085cd3080dc37b1e6c037c5d
sojebsikder <sojebsikder@gmail.com>
2021-01-23 18:47:58 +0600

    main

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+import Home from './Home';
+import About from './About';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                This is header
-            </div>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About Us</Link></li>
+                    </ul>
+                    
+
+                    <switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/about' component={About} />
+                    </switch>
+                    
+                </div>
+            </Router>
         );
     }
 }
